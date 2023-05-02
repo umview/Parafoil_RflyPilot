e0d63e50b2371b7d08ab7866f4aa727d86ce2e11
kcx064 <kcx064@163.com>
2023-05-02 18:34:31 +0800

    update upload.sh

--- tools/upload.sh
+++ tools/upload.sh
@@ -1,1 +1,4 @@
-sshpass -p raspberry scp -r ./rflypilot ../config/rflypilot.txt ../config/calibration.txt ../config/parameter.txt pi@192.168.199.226:/home/pi/RflyPilot_Project/RflyPilot/
+hostname='192.168.199.226'
+password='raspberry'
+sshpass -p ${password} ssh pi@${hostname} 'mkdir -p RflyPilot_Project/RflyPilot/'
+sshpass -p ${password} scp -r ./rflypilot ../config/rflypilot.txt ../config/calibration.txt ../config/parameter.txt pi@${hostname}:/home/pi/RflyPilot_Project/RflyPilot/
