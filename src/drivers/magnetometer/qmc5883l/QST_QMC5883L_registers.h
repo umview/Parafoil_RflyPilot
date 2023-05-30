7b7472b37335187767f004e9e3b630e9031d2707
kcx064 <kcx064@163.com>
2023-05-31 00:11:18 +0800

    QMC5883L set as 100Hz

--- src/drivers/magnetometer/qmc5883l/QST_QMC5883L_registers.h
+++ src/drivers/magnetometer/qmc5883l/QST_QMC5883L_registers.h
@@ -67,7 +67,7 @@ enum CNTL1_BIT : uint8_t {
 	RNG_2G          = Bit5 | Bit4, // 00
 
 	// ODR[3:2]
-	ODR_50HZ        = Bit2,        // 01
+	ODR_50HZ        = Bit3,        // 50Hz: 01  改为100Hz: 10
 
 	// MODE[1:0]
 	Mode_Continuous = Bit0,        // 01
